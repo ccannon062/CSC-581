a35dd8c255b08983c13e1767667879f45338e35a
ccannon062 <caleb.cristoph.cannon@gamil.com>
2025-09-01 16:46:34 -0500

    Final changes.

--- sample-web-page/src/App.jsx
+++ sample-web-page/src/App.jsx
@@ -1,6 +1,9 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import "./index.css";
 
 export default function App() {
@@ -9,6 +12,9 @@ export default function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
